29adced17055d3ca6732854904bea26f57e9e2ba
shacov <98609505+shacov@users.noreply.github.com>
2025-10-31 14:14:40 +0800

    Delete src directory

--- src/main/java/scp002/quickstack/util/Utils.java
+++
@@ -1,9 +0,0 @@
-package scp002.quickstack.util;
-
-import net.minecraft.item.ItemStack;
-
-public class Utils {
-  public static boolean isFavorited(ItemStack stack){
-    return stack.hasTag() && stack.getTag().getBoolean("favorite");
-  }
-}
